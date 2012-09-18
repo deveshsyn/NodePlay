06e2df1e11c684e652fc1f587c224bf35650642b
Devesh Chanchlani <devesh.chanchlani@quickoffice.com>
2012-09-18 11:19:27 +0530

    adding support for serving static content

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+app.use(app.router);
+
 app.get('/', function(req, res){
   res.send('Hello World');
 });
 
 app.listen(3000);
+
 console.log('Express started on port 3000');
