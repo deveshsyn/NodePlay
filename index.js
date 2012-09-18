efe0d8888e0f052df1251a8fa78644853acb8fc3
Devesh Chanchlani <devesh.chanchlani@quickoffice.com>
2012-09-18 20:32:58 +0530

    Adding JADE support

--- index.js
+++ index.js
@@ -11,26 +11,45 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
+function User(name, email) {
+  this.name = name;
+  this.email = email;
+}
+
+// Dummy users
 var users = [
-  { name: 'tobi', age: '10' },
-  { name: 'loki', age: '20' },
-  { name: 'jane', age: '30' }
+  new User('tj', 'tj@vision-media.ca'),
+  new User('ciaran', 'ciaranj@gmail.com'),
+  new User('aaron', 'aaron.heckmann+github@gmail.com')
 ];
 
 /**
  * Express.js as Webservice
  */
-app.get('/api/users/:name', function(req, res, next) {
+app.get('/webservice/:name', function(req, res, next) {
   var userName = req.params.name;
 
   for (var i = 0; i < users.length; i++) {
     var user = users[i];
 
     if (user.name == userName) {
-      return res.send(user);
+      return res.send(user.email);
     }
     return res.send('Name not found.');
   }
+});
+
+/**
+ * Express.js with JADE
+ */
+
+// Optional since express defaults to CWD/views
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'jade');
+
+app.get('/jade/users', function(req, res){
+  res.render('users', { users: users });
 });
 
 app.listen(3000);
